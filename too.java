1790ad86176e89e36368b3e792d29da16bc8d1e6
anteneh <112592567+tuna-11@users.noreply.github.com>
2022-11-20 11:48:35 -0700

    Initial commit

---
+++ too.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class too {
+    public static void Main(String[] args) {
+        String name = "doro";
+        byte age = 23;
+        Date now =new Date();
+
+    }
+}
+
